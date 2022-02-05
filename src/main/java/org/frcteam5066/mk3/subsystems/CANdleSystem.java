4b38e8f473afc946dd48f49ffe7c50f67fae0f6d
criggert <46465700+criggert@users.noreply.github.com>
2022-02-05 16:53:14 -0500

    fixed CANdlesystem class

--- src/main/java/org/frcteam5066/mk3/subsystems/CANdleSystem.java
+++ src/main/java/org/frcteam5066/mk3/subsystems/CANdleSystem.java
@@ -1,12 +1,12 @@
+package org.frcteam5066.mk3.subsystems;
+
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import org.frcteam5066.mk3.Constants;
 
 import com.ctre.phoenix.led.*;
 import com.ctre.phoenix.led.CANdle.LEDStripType;
