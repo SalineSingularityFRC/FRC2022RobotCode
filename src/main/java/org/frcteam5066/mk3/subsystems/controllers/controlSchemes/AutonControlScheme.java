6ec353a6bd1e2d34160d4fc457ccce888785f68d
Max Pfeiffer <maxjpfeiffer@ymail.com>
2022-02-09 20:09:51 -0500

    Max Auton changes

---
+++ src/main/java/org/frcteam5066/mk3/subsystems/controllers/controlSchemes/AutonControlScheme.java
@@ -0,0 +1,116 @@
+package org.frcteam5066.mk3.subsystems.controllers.controlSchemes;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+
+import org.frcteam5066.common.math.Vector2;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.SPI.Port;
+import org.frcteam5066.mk3.LimeLight;
+import org.frcteam5066.mk3.subsystems.DrivetrainSubsystem;
+import org.frcteam5066.mk3.subsystems.Shooter;
+import org.frcteam5066.mk3.subsystems.Intake;
+
+public abstract class AutonControlScheme {
+
+    protected static AHRS gyro;
+    protected static LimeLight limeLight;
+    protected static DrivetrainSubsystem drive;
+    protected static Shooter flywheel;
+    protected static Intake intake;
+    protected static int position;
+    protected static int color;
+    protected static int rotationDirection; //1 is clockwise, -1 is counter-clockwise
+    
+    SendableChooser<Integer> startingPosition = new SendableChooser<>();
+
+    public AutonControlScheme(LimeLight limeLight, Shooter flywheel, DrivetrainSubsystem drive, String color){
+        
+        startingPosition.setDefaultOption("Position 1", 1);
+        startingPosition.addOption("Position 2", 2);
+        startingPosition.addOption("Position 3", 3);
+        startingPosition.addOption("Position 3", 4);
+
+        this.gyro = new AHRS(Port.kMXP);
+        this.limeLight = limeLight;
+        this.flywheel = flywheel;
+       // this.intake = intake;
+        this.position = startingPosition.getSelected();
+        if(color.equals("Red")) this.color = 2;
+        else this.color = 3;
+        if(position < 3) rotationDirection = 1;
+        else rotationDirection = -1;
+    }
+
+    private static boolean hasCargoTarget(){
+        if(color == 2) return limeLight.hasRedCargoTarget();
+        return limeLight.hasBlueCargoTarget();
+    }
+
+    public void drive(){
+        if(position == 1){
+            //I don't know yet
+        }
+
+        else if(position != 1){
+            
+            //use runLimeLight, pass in color as int (2 or 3)
+            limeLight.runLimeLight(drive, color);
+  
+
+        }
+    }
+    
+    
+    public void shoot(){
+        if(position != 1){
+            while(!limeLight.hasVisionTarget()){
+                drive.drive(new Vector2(0, 0), 1 * rotationDirection, false);
+            }
+        }
+
+        limeLight.runLimeLight(drive, 1);
+        flywheel.shooterOn();
+
+        //this next section waits for 1 second to pass. This should be optimized once we get a chance to run this on the robot
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException ex) {
+            
+        }
+
+        intake.conveyorCollect();
+        flywheel.shoot();
+
+        //this will be replaced with color sensor telling us when a ball has been fired
+        try {
+            Thread.sleep(1000);
+        }
+        catch (InterruptedException ex) {
+            
+        }
+
+        intake.conveyorOff();
+        flywheel.shooterOff();
+
+    }
+
+    public void getBall(){
+        double initPos = gyro.getAngle();
+        while(!hasCargoTarget()){
+            drive.drive(new Vector2(0, 0), 1 * rotationDirection, false);
+            if( Math.abs(initPos - gyro.getAngle()) >  180 ) break;
+        }
+        initPos = gyro.getAngle();
+        while(!hasCargoTarget()){
+            drive.drive(new Vector2(0, 0), 1 * -rotationDirection, false);
+            if( Math.abs(initPos - gyro.getAngle()) >  180 ) break;
+        }
+
+        limeLight.runLimeLight(drive, 1);
+
+    }
+
+
+}
---
+++ src/main/java/org/frcteam5066/mk3/subsystems/controllers/controlSchemes/RunAuton
@@ -0,0 +1,40 @@
+package org.frcteam5066.mk3.subsystems.controllers.controlSchemes;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+
+import org.frcteam5066.mk3.LimeLight;
+import org.frcteam5066.mk3.subsystems.DrivetrainSubsystem;
+import org.frcteam5066.mk3.subsystems.Shooter;
+
+public class RunAuton extends AutonControlScheme{
+    
+    SendableChooser<Boolean> doDrive = new SendableChooser<>();
+    SendableChooser<Boolean> doShoot = new SendableChooser<>();
+    SendableChooser<Boolean> doSearch = new SendableChooser<>();
+    SendableChooser<Boolean> doShoot2 = new SendableChooser<>();
+
+    public RunAuton(LimeLight limeLight, Shooter flywheel, DrivetrainSubsystem drive, String color){
+        super(limeLight, flywheel, drive, color);
+
+        doDrive.setDefaultOption("Yes", true);
+        doDrive.addOption("No", false);
+
+        doShoot.setDefaultOption("Yes", true);
+        doShoot.addOption("No", false);
+
+        doSearch.setDefaultOption("Yes", true);
+        doSearch.addOption("No", false);
+
+        doShoot2.setDefaultOption("Yes", true);
+        doShoot2.addOption("No", false);
+    }
+
+    public void moveAuton(){
+        
+        if(doDrive.getSelected()) super.drive();
+        if(doShoot.getSelected()) super.shoot();
+        if(doSearch.getSelected()) super.getBall();
+        if(doShoot2.getSelected()) super.shoot();
+    }
+
+}
